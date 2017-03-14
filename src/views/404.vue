73a37314833873ba70581a3219792df0cbc58934
MisterTaki <ustb_gaoqi@163.com>
2017-03-15 00:26:47 +0800

    初始化

---
+++ src/views/404.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main-container">
+    <h1 class="title">404</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+  };
+</script>
+
+<style scoped lang="scss">
+  @import "../styles/variables";
+  @import "../styles/mixin";
+  .main-container {
+
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main-container">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+  };
+</script>
+
+<style scoped lang="scss">
+  @import "../styles/variables";
+  @import "../styles/mixin";
+  .main-container {
+    height: 100px;
+    background-color: red;
+  }
+</style>
