eab987cf8da48062649724acd8f7d71332c65a69
MisterTaki <ustb_gaoqi@163.com>
2017-03-19 15:52:20 +0800

    添加导航切换效果

--- src/components/User/Layout/Layout.vue
+++ src/components/User/Layout/Layout.vue
@@ -25,16 +25,16 @@
       </div>
     </div>
     <div class="fixed-menu">
-      <router-link to="home" class="menu-item home active" title="主页">主页</router-link>
-      <router-link to="my-teacher" class="menu-item my-teacher" title="我的导师">我的导师</router-link>
-      <router-link to="my-students" class="menu-item my-students" title="我的学生">我的学生</router-link>
-      <router-link to="report-submit" class="menu-item report-submit" title="提交报告">提交报告</router-link>
-      <router-link to="report-review" class="menu-item report-review" title="报告审批">报告审批</router-link>
-      <router-link to="data-share" class="menu-item data-share" title="资料分享">资料分享</router-link>
-      <router-link to="time-schedule" class="menu-item time-schedule" title="时间安排">时间安排</router-link>
-      <router-link to="publish-notice" class="menu-item publish-notice" title="发布公告">发布公告</router-link>
-      <router-link to="account-manage" class="menu-item account-manage" title="账号管理">账号管理</router-link>
-      <router-link to="system-settings" class="menu-item system-settings" title="系统设置">系统设置</router-link>
+      <router-link to="home" class="menu-item home" :class="{ active: currentView === 'home' }" title="主页">主页</router-link>
+      <router-link to="my-teacher" class="menu-item my-teacher" :class="{ active: currentView === 'my-teacher' }" title="我的导师">我的导师</router-link>
+      <router-link to="my-students" class="menu-item my-students" :class="{ active: currentView === 'my-students' }" title="我的学生">我的学生</router-link>
+      <router-link to="submit-report" class="menu-item report-submit" :class="{ active: currentView === 'submit-report' }" title="提交报告">提交报告</router-link>
+      <router-link to="review-report" class="menu-item report-review" :class="{ active: currentView === 'review-report' }" title="报告审批">报告审批</router-link>
+      <router-link to="data-share" class="menu-item data-share" :class="{ active: currentView === 'data-share' }" title="资料分享">资料分享</router-link>
+      <router-link to="reply-schedule" class="menu-item time-schedule" :class="{ active: currentView === 'reply-schedule' }" title="答辩安排">答辩安排</router-link>
+      <router-link to="publish-notice" class="menu-item publish-notice" :class="{ active: currentView === 'publish-notice' }" title="发布公告">发布公告</router-link>
+      <router-link to="account-manage" class="menu-item account-manage" :class="{ active: currentView === 'account-manage' }" title="账号管理">账号管理</router-link>
+      <router-link to="system-settings" class="menu-item system-settings" :class="{ active: currentView === 'system-settings' }" title="系统设置">系统设置</router-link>
     </div>
     <router-view></router-view>
   </div>
@@ -47,6 +47,16 @@
   Vue.use(Badge);
 
   export default {
-    name: 'user-view'
+    name: 'user-view',
+    data () {
+      return {
+        currentView: this.$route.name
+      };
+    },
+    watch: {
+      $route (to) {
+        this.currentView = to.name;
+      }
+    }
   };
 </script>
