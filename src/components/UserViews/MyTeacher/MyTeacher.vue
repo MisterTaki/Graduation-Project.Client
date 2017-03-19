fbafef184723114d84dc293b58e6a60d2d39828d
MisterTaki <ustb_gaoqi@163.com>
2017-03-19 16:40:40 +0800

    完成我的导师静态界面

--- src/components/UserViews/MyTeacher/MyTeacher.vue
+++ src/components/UserViews/MyTeacher/MyTeacher.vue
@@ -7,20 +7,31 @@
       <div class="myTeacher-wrapper">
         <template v-if="status===0">
           <div class="status">
-            <h5 class="notice">还没没有选择导师，点击右下角的“开始选择导师”来开始：）</h5>
-            <el-button type="primary">开始选择导师</el-button>
+            <h5 class="notice">还没有选择导师，点击右下角的“开始选择导师”来开始：）</h5>
+            <router-link to="choose-teacher" class="choose-btn">开始选择导师</router-link>
           </div>
         </template>
         <template v-else-if="status===1">
           <div class="status">
             <h5 class="notice">已提交导师志愿名单，请耐心等待导师选择确认：）</h5>
-            <el-button class="start-btn" type="primary">查看选择志愿</el-button>
+            <el-button class="start-btn" type="text" @click="dialog=true">查看已选择志愿</el-button>
           </div>
+          <el-dialog class="dialog-table" title="已选择志愿" size="small" v-model="dialog">
+            <el-table class="my-volunteer" :data="myVolunteer" stripe border>
+              <el-table-column width="100" type="index" label="志愿次序" align="center"></el-table-column>
+              <el-table-column width="140" prop="name" label="姓名" align="center"></el-table-column>
+              <el-table-column prop="academy" label="学院" align="center"></el-table-column>
+              <el-table-column prop="email" label="邮箱" align="center"></el-table-column>
+              <el-table-column prop="mobile" label="手机" align="center"></el-table-column>
+              <el-table-column prop="topic" label="研究课题" align="center"></el-table-column>
+            </el-table>
+          </el-dialog>
         </template>
         <template v-else-if="status===2">
           <div class="status">
             <el-table class="myTeacher-table" :data="myTeacher" stripe border>
               <el-table-column prop="name" label="姓名" align="center"></el-table-column>
+              <el-table-column prop="academy" label="学院" align="center"></el-table-column>
               <el-table-column prop="email" label="邮箱" align="center"></el-table-column>
               <el-table-column prop="mobile" label="手机" align="center"></el-table-column>
               <el-table-column prop="topic" label="课题" align="center"></el-table-column>
@@ -34,19 +45,41 @@
 
 <script>
   import Vue from 'vue';
-  import { Button, Table, TableColumn } from 'element-ui';
+  import { Button, Table, TableColumn, Dialog } from 'element-ui';
 
   Vue.use(Button);
   Vue.use(Table);
   Vue.use(TableColumn);
+  Vue.use(Dialog);
 
   export default {
     name: 'my-teacher',
     data () {
       return {
         status: 0, // 0:未选择导师， 1:等待导师回复中，2:已选择完成
+        dialog: false,
+        myVolunteer: [{
+          name: '王昭顺',
+          academy: '计算机与通信工程',
+          email: 'zhswang@sohu.com',
+          mobile: '13520555528',
+          topic: '毕业设计（论文）管理系统'
+        }, {
+          name: '王昭顺',
+          academy: '计算机与通信工程',
+          email: 'zhswang@sohu.com',
+          mobile: '13520555528',
+          topic: '毕业设计（论文）管理系统'
+        }, {
+          name: '王昭顺',
+          academy: '计算机与通信工程',
+          email: 'zhswang@sohu.com',
+          mobile: '13520555528',
+          topic: '毕业设计（论文）管理系统'
+        }],
         myTeacher: [{
           name: '王昭顺',
+          academy: '计算机与通信工程',
           email: 'zhswang@sohu.com',
           mobile: '13520555528',
           topic: '毕业设计（论文）管理系统'
