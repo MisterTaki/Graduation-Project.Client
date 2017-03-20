33a385c98772e4c9d3b89fee0daa44b9525afaa1
MisterTaki <ustb_gaoqi@163.com>
2017-03-20 17:03:41 +0800

    :ghost: add 'Upload' component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import { Row, Col, Dialog, Button, Table, TableColumn, Form, FormItem, Select, Option, Input, Radio, RadioGroup, Badge, Step, Steps } from 'element-ui';
+import { Row, Col, Dialog, Upload, Button, Table, TableColumn, Form, FormItem, Select, Option, Input, Radio, RadioGroup, Badge, Step, Steps } from 'element-ui';
 import App from './App';
 import router from './router';
 
@@ -9,6 +9,7 @@ import router from './router';
 Vue.use(Row);
 Vue.use(Col);
 Vue.use(Dialog);
+Vue.use(Upload);
 Vue.use(Button);
 Vue.use(Table);
 Vue.use(TableColumn);
