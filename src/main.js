bf42fb2581d6222a4562000e1e81ab2ac16d045d
MisterTaki <ustb_gaoqi@163.com>
2017-03-26 19:25:29 +0800

    :vhs: add Tabs;

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import { Row, Col, Dialog, Upload, Button, Table, TableColumn, Form, FormItem, Select, Option, Input, Radio, RadioGroup, Badge, Step, Steps } from 'element-ui';
+import { Row, Col, Dialog, Upload, Button, Table, Tabs, TabPane, TableColumn, Form, FormItem, Select, Option, Input, Radio, RadioGroup, Badge, Step, Steps } from 'element-ui';
 import App from './App';
 import router from './router';
 
@@ -11,6 +11,8 @@ Vue.use(Col);
 Vue.use(Dialog);
 Vue.use(Upload);
 Vue.use(Button);
+Vue.use(Tabs);
+Vue.use(TabPane);
 Vue.use(Table);
 Vue.use(TableColumn);
 Vue.use(Form);
