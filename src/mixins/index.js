c4368754dc125000ec8f0375c7f2c2d7f4eda2a1
MisterTaki <ustb_gaoqi@163.com>
2017-03-28 20:30:33 +0800

    :camera: add resetFields() method;

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -4,6 +4,7 @@ export default {
       const forms = formNames instanceof Array ? formNames : [formNames];
       for (let i = 0; i < forms.length; i += 1) {
         this[forms[i]] = this.$options.data()[forms[i]];
+        // this.$refs[forms[i]].resetFields();
       }
     }
   }
