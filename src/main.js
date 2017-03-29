8ebc113bae234aca60ba0ee2ce2fe246ef576f89
MisterTaki <ustb_gaoqi@163.com>
2017-03-29 19:09:46 +0800

    :new_moon_with_face: add 'datePicker';

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import { Row, Col, Dialog, Upload, Button, Table, Tabs, TabPane, TableColumn, Form, FormItem, Select, Option, Input, Radio, RadioGroup, Badge, Step, Steps, Dropdown, DropdownMenu, DropdownItem } from 'element-ui';
+import { Row, Col, Dialog, Upload, Button, Table, Tabs, TabPane, TableColumn, Form, FormItem, Select, Option, Input, Radio, RadioGroup, Badge, Step, Steps, Dropdown, DropdownMenu, DropdownItem, datePicker } from 'element-ui';
 import App from './App';
 import router from './router';
 
@@ -28,6 +28,7 @@ Vue.use(Steps);
 Vue.use(Dropdown);
 Vue.use(DropdownMenu);
 Vue.use(DropdownItem);
+Vue.use(datePicker);
 
 Vue.config.productionTip = false;
 
