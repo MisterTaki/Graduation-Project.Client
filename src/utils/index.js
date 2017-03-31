32baae2d83fbd4cd40d00cde67e148adf042ff70
MisterTaki <ustb_gaoqi@163.com>
2017-03-31 18:48:20 +0800

    :police_car: delete a method;

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,35 +7,3 @@ export function oneOf (value, validList) {
   }
   return false;
 }
-
-// scrollTop animation
-export function scrollTop (el, from = 0, to, duration = 500) {
-  if (!window.requestAnimationFrame) {
-    window.requestAnimationFrame = (window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
-      return window.setTimeout(callback, 1000 / 60);
-    });
-  }
-  const difference = Math.abs(from - to);
-  const step = Math.ceil((difference / duration) * 50);
-
-  function scroll (start, end) {
-    if (start === end) return;
-
-    let d = (start + step > end)
-      ? end
-      : start + step;
-    if (start > end) {
-      d = (start - step < end)
-        ? end
-        : start - step;
-    }
-
-    if (el === window) {
-      window.scrollTo(d, d);
-    } else {
-      // el.scrollTop = d;
-    }
-    window.requestAnimationFrame(() => scroll(d, end));
-  }
-  scroll(from, to);
-}
