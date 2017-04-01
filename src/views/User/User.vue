d212c3865d887e41e1cdb959f6b8863aa91627b3
MisterTaki <ustb_gaoqi@163.com>
2017-04-01 12:50:52 +0800

    :helicopter: update structure；

---
+++ src/views/User/User.vue
@@ -0,0 +1,26 @@
+<style scoped lang="scss" src="./User.scss"></style>
+
+<template lang="html">
+  <div class="main-container">
+    <fixed-header :userName="userName"></fixed-header>
+    <fixed-menu></fixed-menu>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import { Common } from '@/components';
+
+  export default {
+    name: 'user-view',
+    components: {
+      'fixed-header': Common.FixedHeader,
+      'fixed-menu': Common.FixedMenu,
+    },
+    data () {
+      return {
+        userName: '高琦'
+      };
+    }
+  };
+</script>
---
+++ src/views/index.js
@@ -0,0 +1,3 @@
+export Login from './Login/Login';
+export User from './User/User';
+export NotFound from './NotFound/NotFound';
