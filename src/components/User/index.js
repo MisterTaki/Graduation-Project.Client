7558d11478a68fa572292549d44d9434459b709e
MisterTaki <ustb_gaoqi@163.com>
2017-04-01 12:48:55 +0800

    :railway_car: update;

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -1,4 +1,3 @@
-export View from './View/View';
 export * as Common from './Common';
 export * as Student from './Student';
 export * as Teacher from './Teacher';
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,2 @@
 export * as Common from './Common';
-export Login from './Login/Login';
 export * as User from './User';
-export NotFound from './NotFound/NotFound';
