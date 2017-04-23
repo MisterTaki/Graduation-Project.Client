b5e922f34787012f07788fe7e1a92b034ca9ca42
MisterTaki <ustb_gaoqi@163.com>
2017-04-23 23:46:57 +0800

    :gem: add '/user/load';

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,8 @@
 export default {
-  applyUser: '/user/apply',
-  createUser: '/user/create',
-  updateUser: '/user/update',
+  load: '/user/load',
+  apply: '/user/apply',
+  create: '/user/create',
+  update: '/user/update',
   modifyPwd: '/user/modify-pwd',
   forgetPwd: '/user/forget-pwd',
   setPwd: '/user/set-pwd',
