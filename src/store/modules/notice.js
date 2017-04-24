fa972e62361838817b30fe51c4b8a373b8d3aedf
MisterTaki <ustb_gaoqi@163.com>
2017-04-24 20:09:37 +0800

    .publishNotice-view--table add load, remove, delete;

--- src/store/modules/notice.js
+++ src/store/modules/notice.js
@@ -2,15 +2,34 @@ import { client } from '@/helpers';
 import { notice } from '@/api';
 
 const PUBLISH = 'notice/PUBLISH';
+const LOAD = 'notice/LOAD';
+const REMOVE = 'notice/REMOVE';
+const DELETE = 'notice/DELETE';
 
 export default {
   state: {
+    data: []
   },
   actions: {
     async [PUBLISH] ({ commit }, data) {
-      await client.post(notice.publish, { data });
+      commit(PUBLISH, await client.post(notice.publish, { data }));
+    },
+    async [LOAD] ({ commit }) {
+      commit(LOAD, await client.get(notice.load));
+    },
+    async [REMOVE] ({ commit }, data) {
+      await client.delete(notice.remove, { data });
     }
   },
   mutations: {
+    [LOAD] (state, data) {
+      state.data = data.noticeList;
+    },
+    [DELETE] (state, data) {
+      state.data.splice(data.index, 1);
+    },
+    [PUBLISH] (state, data) {
+      state.data.push(data.notice);
+    },
   }
 };
