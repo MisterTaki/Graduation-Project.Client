e281de2e43af216ebee0f95c3053b4e1b9004260
MisterTaki <ustb_gaoqi@163.com>
2017-04-24 15:28:52 +0800

    :scroll: add MODIFY_EMAIL, SET_EMAIL;

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -8,6 +8,8 @@ const UPDATE = 'user/UPDATE';
 const FORGET_PASSWORD = 'user/FORGET_PASSWORD';
 const SET_PASSWORD = 'user/SET_PASSWORD';
 const MODIFY_PASSWORD = 'user/MODIFY_PASSWORD';
+const MODIFY_EMAIL = 'user/MODIFY_EMAIL';
+const SET_EMAIL = 'user/SET_EMAIL';
 
 export default {
   state: {
@@ -35,10 +37,19 @@ export default {
     async [SET_PASSWORD] ({ commit }, data) {
       await client.post(user.setPwd, { data });
     },
+    async [MODIFY_EMAIL] ({ commit }, data) {
+      await client.post(user.modifyEmail, { data });
+    },
+    async [SET_EMAIL] ({ commit }, data) {
+      commit(SET_EMAIL, await client.post(user.setEmail, { data }));
+    },
   },
   mutations: {
     [LOAD] (state, data) {
       state.info = data;
     },
+    [SET_EMAIL] (state, data) {
+      state.info.email = data.newEmail;
+    }
   }
 };
