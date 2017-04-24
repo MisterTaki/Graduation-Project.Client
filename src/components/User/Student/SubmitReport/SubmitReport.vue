f29bb5bb2bafe0af5263978bbd0cb1627c7680db
MisterTaki <ustb_gaoqi@163.com>
2017-04-24 15:27:35 +0800

    :triangular_ruler: load status;

--- src/components/User/Student/SubmitReport/SubmitReport.vue
+++ src/components/User/Student/SubmitReport/SubmitReport.vue
@@ -6,16 +6,20 @@
       <div class="submit-wrapper">
         <div class="title-wrapper">
           <h2 class="title">提交报告</h2>
-          <el-button class="upload-btn" type="text" @click="dialog.upload=true">上传<i class="el-icon-upload2 el-icon--right"></i></el-button>
+          <template v-if="systemStatus !== 0">
+            <el-button class="upload-btn" type="text" @click="dialog.upload=true">上传<i class="el-icon-upload2 el-icon--right"></i></el-button>
+          </template>
         </div>
-        <h5 class="notice">同学你好，现在是提交“{{notice[status]}}”环节，请点击右上角上传按钮进行提交：）</h5>
-        <el-dialog class="upload-dialog" title="报告上传提交" size="small" v-model="dialog.upload">
-          <el-upload ref="upload" class="upload submitReport-view--upload" action="//jsonplaceholder.typicode.com/posts/" drag :before-upload="beforeUpload" :on-success="submitSuccess">
-            <i class="el-icon-upload"></i>
-            <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-            <div class="el-upload__tip" slot="tip">只能上传doc/docx文件，且不超过5MB</div>
-          </el-upload>
-        </el-dialog>
+        <h5 class="notice">同学你好，现在是“{{noticeText}}”环节，{{systemStatus !== 0 ? '请点击右上角上传按钮进行提交：）' : '暂时无法提交报告'}}</h5>
+        <template v-if="dialog.upload">
+          <el-dialog class="upload-dialog" title="报告上传提交" size="small" v-model="dialog.upload">
+            <el-upload ref="upload" class="upload submitReport-view--upload" action="//jsonplaceholder.typicode.com/posts/" drag :before-upload="beforeUpload" :on-success="submitSuccess">
+              <i class="el-icon-upload"></i>
+              <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+              <div class="el-upload__tip" slot="tip">只能上传doc/docx文件，且不超过5MB</div>
+            </el-upload>
+          </el-dialog>
+        </template>
       </div>
       <div class="record-wrapper">
         <h2 class="title">提交记录</h2>
@@ -32,19 +36,14 @@
 </template>
 
 <script>
+  import store from '@/store';
+  import { mapState } from 'vuex';
   import { Message } from 'element-ui';
 
   export default {
     name: 'submit-report',
     data () {
       return {
-        status: 0,
-        notice: [
-          '任务书',
-          '选题报告',
-          '中期报告',
-          '最终毕业设计（论文）'
-        ],
         recordsData: [
           {
             time: '2016-06-06',
@@ -60,6 +59,19 @@
         }
       };
     },
+    computed: mapState({
+      systemStatus: ({ system }) => system.status,
+      noticeText () {
+        const noticeTexts = [
+          '与导师双向选择',
+          '任务书',
+          '选题报告',
+          '中期报告',
+          '最终毕业设计（论文）'
+        ];
+        return noticeTexts[this.systemStatus - 1];
+      }
+    }),
     methods: {
       beforeUpload (file) {
         const extension = file.name.slice(file.name.lastIndexOf('.'));
@@ -77,6 +89,15 @@
         console.log(file.name);
         Message.success('提交报告成功');
       }
+    },
+    beforeRouteEnter (to, from, next) {
+      if (!store.state.system.status) {
+        store.dispatch('system/GET_STATUS').then(
+          () => next(),
+          () => next(false)
+        );
+      }
+      next();
     }
   };
 </script>
