bd2a294545e2325ba49708e34de315f12ad2cb07
MisterTaki <ustb_gaoqi@163.com>
2017-05-08 14:31:00 +0800

    :postbox: add badge;

--- src/components/Common/FixedHeader/FixedHeader.vue
+++ src/components/Common/FixedHeader/FixedHeader.vue
@@ -10,17 +10,15 @@
     </div>
     <div class="nav-wrapper">
       <router-link to="message" class="nav-item message" title="进入私信中心">
-        <el-badge :value="5" :max="10" class="message-badge"></el-badge>
-        <div class="message-wrapper">
-          <h5 class="message-title">私信（最新5条）</h5>
-          <ul class="message-list">
-            <li class="message-item">这是一条私信</li>
-            <li class="message-item">这是一条私信</li>
-            <li class="message-item">这是一条私信</li>
-            <li class="message-item">这是一条私信</li>
-            <li class="message-item">这是一条私信</li>
-          </ul>
-        </div>
+        <el-badge :value="badgeLen" :max="10" class="message-badge"></el-badge>
+        <template v-if="badgeLen > 0">
+          <div class="message-wrapper">
+            <h5 class="message-title">未读私信{{isMore ? '（最新10条）': ''}}</h5>
+            <ul class="message-list">
+              <li class="message-item" v-for="(item, index) in latestMessages" :key="index">{{item.title}}</li>
+            </ul>
+          </div>
+        </template>
       </router-link>
       <router-link to="user-settings" class="nav-item settings" title="进入个人设置"></router-link>
       <div @click="logout" class="nav-item logout" title="注销"></div>
@@ -35,8 +33,24 @@
 
   export default {
     name: 'fixed-header',
+    data () {
+      return {
+        badgeLen: 0,
+        isMore: false
+      };
+    },
     computed: mapState({
-      username: ({ auth }) => auth.username
+      username: ({ auth }) => auth.username,
+      latestMessages ({ message }) {
+        const messages = message.latest.value;
+        this.badgeLen = messages.length;
+        if (messages.length > 10) {
+          this.isMore = true;
+          return messages.slice(0, 10);
+        }
+        this.isMore = false;
+        return messages;
+      }
     }),
     methods: {
       logout () {
@@ -46,6 +60,10 @@
           router.push('/login');
         }, () => Message.error('注销失败'));
       }
-    }
+    },
+    created () {
+      if (this.$store.state.message.latest.loaded) return;
+      this.$store.dispatch('message/LOAD_LATEST');
+    },
   };
 </script>
