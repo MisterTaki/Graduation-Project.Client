50b4fada2ad9562aa69ff993e3df94e2acdc8f64
MisterTaki <ustb_gaoqi@163.com>
2017-05-08 16:13:28 +0800

    :calendar: add 'root/RESET';

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,26 @@ import * as modules from './modules';
 
 Vue.use(Vuex);
 
+const RESET = 'root/RESET';
+const initializeState = {};
+
+/* eslint no-restricted-syntax: 0 */
+for (const key in modules) {
+  if (Object.prototype.hasOwnProperty.call(modules, key)) {
+    initializeState[key] = JSON.parse(JSON.stringify(modules[key].state));
+  }
+}
+
 export default new Vuex.Store({
+  mutations: {
+    [RESET] (state) {
+      for (const key in initializeState) {
+        if (Object.prototype.hasOwnProperty.call(modules, key)) {
+          state[key] = JSON.parse(JSON.stringify(initializeState[key]));
+        }
+      }
+    }
+  },
   modules,
   strict: process.env.NODE_ENV !== 'production'
 });
