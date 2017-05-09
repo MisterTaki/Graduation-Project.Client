62fa70825a8256d276ce23c628817feb1dcdb36d
MisterTaki <ustb_gaoqi@163.com>
2017-05-09 10:51:29 +0800

    :headphones: update;

--- src/components/User/Student/ChooseTeacher/ChooseTeacher.vue
+++ src/components/User/Student/ChooseTeacher/ChooseTeacher.vue
@@ -8,15 +8,15 @@
         <el-button class="volunteerList-btn" type="text" @click="dialog.volunteerList=true">我的志愿列表<i class="el-icon-document el-icon--right"></i></el-button>
       </div>
       <div class="teacherList-wrapper">
-        <el-table class="teacher-list chooseTeacher-view--table" :data="teachersData" border>
+        <el-table class="teacher-list chooseTeacher-view--table" :data="teacherOptions" border>
           <el-table-column type="expand">
             <template scope="props">
               <el-form label-position="left" label-width="100px">
                 <el-form-item label="导师姓名：">
-                  <span>{{ props.row.name }}</span>
+                  <span>{{ props.row.username }}</span>
                 </el-form-item>
                 <el-form-item label="性别：">
-                  <span>{{ props.row.gender }}</span>
+                  <span>{{ props.row.gender === 'm' ? '男' : '女' }}</span>
                 </el-form-item>
                 <el-form-item label="学历：">
                   <span>{{ props.row.education }}</span>
@@ -33,9 +33,9 @@
               </el-form>
             </template>
           </el-table-column>
-          <el-table-column width="120" prop="name" label="姓名" align="center"></el-table-column>
-          <el-table-column width="180" prop="academy" :filters="academyList" :filter-method="filterAcademy" label="学院" align="center"></el-table-column>
-          <el-table-column prop="topicList" :formatter="formatterTopic" label="提供的研究课题" align="center"></el-table-column>
+          <el-table-column width="120" prop="username" label="姓名" align="center"></el-table-column>
+          <el-table-column width="180" prop="academy" :formatter="formatterAcademy" :filters="academyValues" :filter-method="filterAcademy" label="学院" align="center"></el-table-column>
+          <el-table-column prop="topics" :formatter="formatterTopic" label="提供的研究课题" align="center"></el-table-column>
           <el-table-column width="140" label="操作" align="center">
             <template scope="scope">
               <el-button type="text" size="small" @click="addVolunteer(scope.$index, scope.row)">添加至志愿列表</el-button>
@@ -71,7 +71,7 @@
           </template>
         </el-table-column>
         <el-table-column width="120" prop="order" label="志愿次序" align="center"></el-table-column>
-        <el-table-column width="120" prop="name" label="导师姓名" align="center"></el-table-column>
+        <el-table-column width="120" prop="username" label="导师姓名" align="center"></el-table-column>
         <el-table-column prop="choosedTopic" label="选择的研究课题" align="center"></el-table-column>
         <el-table-column width="140" label="操作" align="center">
           <template scope="scope">
@@ -80,19 +80,19 @@
         </el-table-column>
       </el-table>
       <div class="submitTopic-btn-wrapper">
-        <el-button v-if="volunteersForm.length !== 0" type="primary" @click="submitTopic">提交志愿</el-button>
+        <el-button v-if="volunteersForm.length !== 0" type="primary" @click="submitTopic" :loading="loading">提交志愿</el-button>
       </div>
     </el-dialog>
     <el-dialog class="chooseTopic-dialog" title="选择研究课题" size="tiny" v-model="dialog.chooseTopic" :close-on-click-modal=false>
-      <el-form ref="chooseTopic-form" :model="chooseTopicForm">
+      <el-form ref="chooseTopicForm" :model="chooseTopicForm">
         <el-form-item>
           <el-select v-model="chooseTopicForm.choosedTopic" placeholder="请选择研究课题" style="width: 100%">
-            <el-option v-for="item in candidateTopics" :label="item" :value="item"></el-option>
+            <el-option v-for="(item,index) in candidateTopics" :label="item" :value="item" :key="index"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item>
-          <el-select v-model="chooseTopicForm.order" placeholder="请选择此志愿为第几志愿" style="width: 100%">
-            <el-option v-for="item in candidateOrders" :label="item" :value="item"></el-option>
+          <el-select v-model="chooseTopicForm.order" placeholder="请选择此志愿次序" style="width: 100%">
+            <el-option v-for="item in orderOptions" :label="item.label" :value="item.value" :key="item.value"></el-option>
           </el-select>
         </el-form-item>
         <el-button type="primary" style="width: 100%" @click="addTopic">添加志愿</el-button>
@@ -102,7 +102,9 @@
 </template>
 
 <script>
+  import { mapState } from 'vuex';
   import { Message } from 'element-ui';
+  import store from '@/store';
   import mixins from '@/mixins';
 
   export default {
@@ -110,121 +112,130 @@
     mixins: [mixins],
     data () {
       return {
-        academyList: [
-          {
-            text: '经济与管理',
-            value: '经济与管理'
-          }, {
-            text: '计算机与通信工程',
-            value: '计算机与通信工程'
-          },
-        ],
-        teachersData: [
-          {
-            name: '导师1',
-            gender: '男',
-            academy: '计算机与通信工程',
-            education: '博士',
-            position: '教授（博导）',
-            email: 'zhswang@sohu.com',
-            mobile: '13520555528',
-            topicList: ['毕业设计（论文）管理系统', '毕业设计（论文）管理系统']
-          }, {
-            name: '导师2',
-            gender: '男',
-            academy: '经济与管理',
-            education: '博士',
-            position: '教授（博导）',
-            email: 'zhswang@sohu.com',
-            mobile: '13520555528',
-            topicList: ['毕业设计（论文）管理系统', '毕业设计（论文）管理系统', '毕业设计（论文）管理系统']
-          },
-          {
-            name: '导师3',
-            gender: '男',
-            academy: '经济与管理',
-            education: '博士',
-            position: '教授（博导）',
-            email: 'zhswang@sohu.com',
-            mobile: '13520555528',
-            topicList: ['毕业设计（论文）管理系统']
-          }
-        ],
         dialog: {
           chooseTopic: false,
           volunteerList: false
         },
         chooseTopicForm: {
-          choosedTopic: '',
           currentData: {},
+          choosedTopic: '',
           order: ''
         },
         candidateTopics: [],
-        candidateOrders: ['第一志愿', '第二志愿', '第三志愿'],
-        // candidateOrders: [
-        //   {
-        //     label: '第一志愿',
-        //     value: 1
-        //   },
-        //   {
-        //     label: '第二志愿',
-        //     value: 2
-        //   },
-        //   {
-        //     label: '第三志愿',
-        //     value: 3
-        //   },
-        // ],
+        orderOptions: [
+          {
+            label: '第一志愿',
+            value: 1
+          },
+          {
+            label: '第二志愿',
+            value: 2
+          },
+          {
+            label: '第三志愿',
+            value: 3
+          },
+        ],
         volunteersForm: [],
       };
     },
+    computed: mapState({
+      loading: ({ global }) => global.loading,
+      academyList: ({ global }) => global.academy.value,
+      teacherOptions: ({ volunteer }) => volunteer.teacherOptions.value,
+      academyValues () {
+        return this.academyList.map(item => ({
+          text: item.value,
+          value: item._id
+        }));
+      }
+    }),
     methods: {
+      formatterAcademy (row) {
+        return this.academyList[row.academyID - 1].value;
+      },
       formatterTopic (row) {
-        return row.topicList.join('；');
+        return row.topics.length > 0 ? row.topics.join('；') : '暂未提供课题';
       },
       filterAcademy (value, row) {
-        return row.academy === value;
+        return row.academyID === value;
       },
       addTopic () {
-        if (this.chooseTopicForm.choosedTopic === '') {
-          Message.error('请选择研究课题');
-          return;
-        } else if (this.chooseTopicForm.order === '') {
-          Message.error('请选择志愿次序');
+        if (this.chooseTopicForm.choosedTopic === '' || this.chooseTopicForm.order === '') {
+          this.showInvalidateMsg();
           return;
         }
         this.chooseTopicForm.currentData.choosedTopic = this.chooseTopicForm.choosedTopic;
         this.chooseTopicForm.currentData.order = this.chooseTopicForm.order;
         this.volunteersForm.push(this.chooseTopicForm.currentData);
-        this.candidateOrders.splice(this.candidateOrders.indexOf(this.chooseTopicForm.order), 1);
+        for (let i = 0; i < this.orderOptions.length; i += 1) {
+          if (this.orderOptions[i].value === this.chooseTopicForm.order) {
+            this.orderOptions.splice(i, 1);
+            break;
+          }
+        }
+        Message.closeAll();
         Message.success('已经成功添加至志愿列表，请在我的志愿列表查看');
         this.resetForm('chooseTopicForm');
         this.dialog.chooseTopic = false;
       },
       addVolunteer (index, row) {
-        if (this.volunteersForm.length === 3) {
+        if (row.topics.length === 0) {
+          Message.closeAll();
+          Message.error('该老师暂未提供课题，无法添加');
+          return;
+        } else if (this.volunteersForm.length === 3) {
+          Message.closeAll();
           Message.error('已经达到最大志愿数（3个），请在我的志愿列表查看');
           return;
         } else if (this.volunteersForm.indexOf(row) > -1) {
+          Message.closeAll();
           Message.warning('已经添加过此志愿老师，请在我的志愿列表查看');
           return;
         }
-        this.candidateTopics = row.topicList;
+        this.candidateTopics = row.topics;
+        this.resetForm('chooseTopicForm');
         this.dialog.chooseTopic = true;
         this.chooseTopicForm.currentData = row;
       },
       removeVolunteer (index, row) {
-        this.candidateOrders.push(row.order);
+        let optionsLabel;
+        switch (row.order) {
+          case 1:
+            optionsLabel = '第一志愿';
+            break;
+          case 2:
+            optionsLabel = '第二志愿';
+            break;
+          case 3:
+            optionsLabel = '第三志愿';
+            break;
+          default:
+        }
+        this.orderOptions.push({
+          label: optionsLabel,
+          value: row.order
+        });
+        this.orderOptions.sort((a, b) => a.value - b.value);
         this.volunteersForm.splice(this.volunteersForm.indexOf(row), 1);
+        Message.closeAll();
         Message.success('移除成功');
       },
       submitTopic () {
-        const data = {
-          data: this.volunteersForm
-        };
+        Message.closeAll();
         Message.success('提交成功');
-        console.log(data);
       }
+    },
+    beforeRouteEnter (to, from, next) {
+      const loads = [];
+      if (!store.state.global.academy.loaded) loads.push(store.dispatch('global/LOAD_ACADEMY'));
+      if (!store.state.volunteer.teacherOptions.loaded) loads.push(store.dispatch('volunteer/LOAD_TEACHER_OPTIONS'));
+      if (loads.length > 0) {
+        return Promise.all(loads)
+          .then(() => next())
+          .catch(() => next(false));
+      }
+      return next();
     }
   };
 </script>
