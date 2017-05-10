a4efd6ed64bd62179cfcfd10dfacc1e43e0e7862
MisterTaki <ustb_gaoqi@163.com>
2017-05-10 15:41:12 +0800

    :bookmark: update;

--- src/components/User/Teacher/MyStudents/MyStudents.vue
+++ src/components/User/Teacher/MyStudents/MyStudents.vue
@@ -5,15 +5,15 @@
     <div class="main">
       <div class="title-wrapper">
         <h2 class="title">我的学生</h2>
-        <template v-if="status !== 1">
-          <el-button class="addTopic-btn" type="text" @click="dialog.topic=true">我的课题<i class="el-icon-fa-tasks el-icon--right"></i></el-button>
+        <template v-if="systemStatus === 1 && confirmedStudents.length !== 5">
+          <el-button class="addTopic-btn" type="text" @click="openMyTopics">我的课题<i class="el-icon-fa-tasks el-icon--right"></i></el-button>
         </template>
         <template v-if="dialog.topic">
           <el-dialog title="我的课题" v-model="dialog.topic" :close-on-click-modal=false>
             <h5 v-show="topicList.topics.length > 0" class="form-title">已提供课题</h5>
             <el-form :model="topicList" ref="topicList" label-width="70px">
               <el-form-item v-for="(topic, index) in topicList.topics" :label="'课题' + (index + 1)" :key="index">
-                <el-input :value="topic" readonly=true></el-input>
+                <el-input :value="topic" :readonly=true></el-input>
               </el-form-item>
             </el-form>
             <h5 class="form-title">新增加课题</h5>
@@ -41,47 +41,43 @@
         </template>
       </div>
       <div class="myTeacher-wrapper">
-        <template v-if="status===0">
+        <template v-if="systemStatus===1">
           <div class="status">
-            <h5 class="notice">还没有选择学生，点击右下角的“开始选择学生”来开始：）</h5>
-            <router-link to="choose-students" class="choose-btn">开始选择学生</router-link>
+            <h5 class="notice">{{ noticeText }}</h5>
+            <router-link to="choose-students" class="choose-btn">{{ confirmedStudents.length !== 5 ? '开始选择学生' : '查看其他志愿学生'}}</router-link>
           </div>
         </template>
-        <template v-else-if="status===1">
-          <div class="status">
-            <el-table class="myStudents-table myStudents-view--table" :data="myStudents" border>
-              <el-table-column type="expand">
-                <template scope="props">
-                  <el-form label-position="left" label-width="100px">
-                    <el-form-item label="学号：">
-                      <span>{{ props.row.studentID }}</span>
-                    </el-form-item>
-                    <el-form-item label="性别：">
-                      <span>{{ props.row.gender }}</span>
-                    </el-form-item>
-                    <el-form-item label="班级：">
-                      <span>{{ props.row.class }}</span>
-                    </el-form-item>
-                    <el-form-item label="专业：">
-                      <span>{{ props.row.major }}</span>
-                    </el-form-item>
-                    <el-form-item label="学院：">
-                      <span>{{ props.row.academy }}</span>
-                    </el-form-item>
-                    <el-form-item label="邮箱：">
-                      <span>{{ props.row.email }}</span>
-                    </el-form-item>
-                    <el-form-item label="手机：">
-                      <span>{{ props.row.mobile }}</span>
-                    </el-form-item>
-                  </el-form>
-                </template>
-              </el-table-column>
-              <el-table-column width="140" prop="name" label="姓名" align="center"></el-table-column>
-              <el-table-column prop="choosedTopic" label="选择的研究课题" align="center"></el-table-column>
-            </el-table>
-          </div>
-        </template>
+        <div class="status">
+          <el-table class="myStudents-table myStudents-view--table" :data="confirmedStudents" border>
+            <el-table-column type="expand">
+              <template scope="props">
+                <el-form label-position="left" label-width="100px">
+                  <el-form-item label="学号：">
+                    <span>{{ props.row._id }}</span>
+                  </el-form-item>
+                  <el-form-item label="性别：">
+                    <span>{{ props.row.gender === 'm' ? '男' : '女' }}</span>
+                  </el-form-item>
+                  <el-form-item label="专业：">
+                    <span>{{ props.row.major }}</span>
+                  </el-form-item>
+                  <el-form-item label="学院：">
+                    <span>{{ props.row.academy }}</span>
+                  </el-form-item>
+                  <el-form-item label="邮箱：">
+                    <span>{{ props.row.email }}</span>
+                  </el-form-item>
+                  <el-form-item label="手机：">
+                    <span>{{ props.row.mobile }}</span>
+                  </el-form-item>
+                </el-form>
+              </template>
+            </el-table-column>
+            <el-table-column width="140" prop="username" label="姓名" align="center"></el-table-column>
+            <el-table-column width="120" prop="_class" label="班级" align="center"></el-table-column>
+            <el-table-column prop="topic" label="选择的研究课题" align="center"></el-table-column>
+          </el-table>
+        </div>
       </div>
     </div>
   </div>
@@ -98,48 +94,40 @@
     mixins: [mixins],
     data () {
       return {
-        status: 0, // 0:开始选择学生，1:已选择完成
         dialog: {
           topic: false
         },
         topicForm: {
           topics: ['']
-        },
-        myStudents: [
-          {
-            name: '高琦',
-            studentID: '41355025',
-            gender: '男',
-            class: '计1301',
-            major: '计算机科学与技术',
-            academy: '计算机与通信工程',
-            email: 'ustb_gaoqi@163.com',
-            mobile: '15652063671',
-            choosedTopic: '毕业设计（论文）管理系统'
-          },
-          {
-            name: '高琦',
-            studentID: '41355025',
-            gender: '男',
-            class: '计1301',
-            major: '计算机科学与技术',
-            academy: '计算机与通信工程',
-            email: 'ustb_gaoqi@163.com',
-            mobile: '15652063671',
-            choosedTopic: '毕业设计（论文）管理系统'
-          },
-        ],
+        }
       };
     },
     computed: mapState({
       loading: ({ global }) => global.loading,
+      academyList: ({ global }) => global.academy.value,
+      systemStatus: ({ system }) => system.status.value,
       topicList ({ topic }) {
         return {
           topics: topic.data.value
         };
+      },
+      confirmedStudents: ({ volunteer }) => volunteer.confirmedStudents.value,
+      noticeText () {
+        if (this.confirmedStudents.length === 5) {
+          return '已经选够5名学生，无法继续选择学生：）';
+        }
+        return `还可以继续选择 ${5 - this.confirmedStudents.length} 名学生，点击右下角的“开始选择学生”来开始：）`;
       }
     }),
     methods: {
+      openMyTopics () {
+        if (!store.state.topic.data.loaded) {
+          store.dispatch('topic/LOAD')
+          .then(() => (this.dialog.topic = true))
+          .catch(() => false);
+        }
+        this.dialog.topic = true;
+      },
       addTopicItem () {
         this.topicForm.topics.push('');
       },
@@ -165,8 +153,12 @@
       },
     },
     beforeRouteEnter (to, from, next) {
-      if (!store.state.topic.data.loaded) {
-        return store.dispatch('topic/LOAD')
+      const loads = [];
+      if (!store.state.global.academy.loaded) loads.push(store.dispatch('global/LOAD_ACADEMY'));
+      if (!store.state.system.status.loaded) loads.push(store.dispatch('system/GET_STATUS'));
+      if (!store.state.volunteer.confirmedStudents.loaded) loads.push(store.dispatch('volunteer/LOAD_CONFIRMED_STUDENTS'));
+      if (loads.length > 0) {
+        return Promise.all(loads)
           .then(() => next())
           .catch(() => next(false));
       }
