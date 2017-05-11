7c91a8b0a40a6d7c058313b417a90738978361c8
MisterTaki <ustb_gaoqi@163.com>
2017-05-11 20:50:52 +0800

    :newspaper: update;

--- src/components/User/Student/SubmitReport/SubmitReport.vue
+++ src/components/User/Student/SubmitReport/SubmitReport.vue
@@ -10,8 +10,8 @@
         </div>
         <h5 class="notice">同学你好，现在是“{{noticeText}}”环节，{{systemStatus !== 1 ? '请点击右上角上传按钮进行提交：）' : '暂时无法提交报告'}}</h5>
         <template v-if="dialog.upload">
-          <el-dialog class="upload-dialog" title="报告上传提交" size="small" v-model="dialog.upload">
-            <el-upload ref="upload" class="upload submitReport-view--upload" action="//jsonplaceholder.typicode.com/posts/" drag :before-upload="beforeUpload" :on-success="submitSuccess">
+          <el-dialog class="upload-dialog" title="报告上传提交" size="small" v-model="dialog.upload" @close="$refs.upload.clearFiles()" :close-on-click-modal=false>
+            <el-upload ref="upload" class="upload submitReport-view--upload" :action="uploadAction" drag :before-upload="beforeUpload" :on-success="submitSuccess" :headers="headers" :data="uploadData">
               <i class="el-icon-upload"></i>
               <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
               <div class="el-upload__tip" slot="tip">只能上传doc/docx文件，且不超过5MB</div>
@@ -19,13 +19,14 @@
           </el-dialog>
         </template>
       </div>
-      <div class="record-wrapper">
+      <div v-if="systemStatus !== 1" class="record-wrapper">
         <h2 class="title">提交记录</h2>
         <div class="recordList-wrapper">
-          <el-table class="record-list" :data="recordsData" border>
+          <el-table class="record-list" :data="reports" border>
             <el-table-column type="index" label="序号" width="70" align="center"></el-table-column>
-            <el-table-column width="120" prop="time" label="提交时间" align="center"></el-table-column>
-            <el-table-column prop="name" label="提交的文件名称" align="center"></el-table-column>
+            <el-table-column width="180" prop="created_at" label="提交时间" align="center"></el-table-column>
+            <el-table-column prop="reportName" label="提交的文件名称" align="center"></el-table-column>
+            <el-table-column width="120" prop="reportSize" label="大小" align="center"></el-table-column>
           </el-table>
         </div>
       </div>
@@ -37,28 +38,29 @@
   import store from '@/store';
   import { mapState } from 'vuex';
   import { Message } from 'element-ui';
+  import { report as reportAPI } from '@/api';
+  import config from '@/config';
 
   export default {
     name: 'submit-report',
     data () {
       return {
-        recordsData: [
-          {
-            time: '2016-06-06',
-            name: '任务书'
-          },
-          {
-            time: '2016-07-07',
-            name: '选题报告'
-          },
-        ],
+        uploadAction: `${config.baseURL}${reportAPI.upload}`,
+        uploadData: {
+          reportSize: '',
+          reporName: ''
+        },
         dialog: {
           upload: false
         }
       };
     },
     computed: mapState({
+      headers: ({ auth }) => ({
+        Authorization: auth.token
+      }),
       systemStatus: ({ system }) => system.status.value,
+      reports: ({ report }) => report.data.value,
       noticeText () {
         const noticeTexts = [
           '与导师双向选择',
@@ -76,26 +78,43 @@
         const isDocument = extension === '.doc' || extension === '.docx';
         const isLt5M = file.size / 1024 / 1024 < 5;
         if (!isDocument) {
+          Message.closeAll();
           Message.error('文件格式错误');
+          return false;
         }
         if (!isLt5M) {
+          Message.closeAll();
           Message.error('文件大小超过5MB');
+          return false;
         }
-        return isDocument && isLt5M;
+        this.uploadData.reportName = file.name;
+        this.uploadData.reportSize = `${(file.size / 1024 / 1024).toFixed(2)}.MB`;
+        return true;
       },
       submitSuccess (response, file) {
-        console.log(file.name);
-        Message.success('提交报告成功');
+        Message.closeAll();
+        Message.success(`提交报告"${file.name}"成功`);
       }
     },
     beforeRouteEnter (to, from, next) {
-      if (!store.state.system.status) {
-        store.dispatch('system/GET_STATUS').then(
-          () => next(),
-          () => next(false)
-        );
+      if (!store.state.system.status.loaded) {
+        return store.dispatch('system/GET_STATUS')
+          .then(() => {
+            if (store.state.system.status.value !== 1) {
+              return store.dispatch('report/LOAD')
+                .then(() => next())
+                .catch(() => next(false));
+            }
+            return next();
+          })
+          .catch(() => next(false));
       }
-      next();
+      if (store.state.system.status.value !== 1) {
+        return store.dispatch('report/LOAD')
+          .then(() => next())
+          .catch(() => next(false));
+      }
+      return next();
     }
   };
 </script>
