9c5d1328f8420ba7f135f23dca1ad750595477f3
MisterTaki <ustb_gaoqi@163.com>
2017-05-12 20:37:18 +0800

    :euro: update;

--- src/api/account.js
+++ src/api/account.js
@@ -1,5 +1,7 @@
 export default {
   load: '/account/load',
+  agree: '/account/agree',
+  refuse: '/account/refuse',
   modify: '/account/modify',
   create: '/account/create',
   delete: '/account/delete',
